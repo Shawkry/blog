1c27c24a936c670dea4f62c083007e0adec3b080
shawkry <shawkrycao@gmail.com>
2023-10-13 20:57:40 +0800

    feat: 新增欢迎页

---
+++ src/components/WelcomePage/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "@site/src/pages/index.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+
+export const WelcomePage = () => {
+  return (
+    <div className="hero">
+      <div className={styles.welcome_intro}>
+        <h1 className={styles.hero_title}>
+          <span style={{ color: "var(--ifm-color-primary)" }}>Shawkry</span> ~
+          <div>略带文艺的小码农</div>
+        </h1>
+        <p className="hero__subtitle">用技术让生活变得更简单 🧐</p>
+      </div>
+      <div className={styles.welcome_svg}>
+        <img src={useBaseUrl("/img/prince.gif")} alt={""} />
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,16 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import Layout from "@theme/Layout";
+import Head from "@docusaurus/Head";
+import { WelcomePage } from "../components/WelcomePage";
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+export default function Home() {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+    <Layout description="用技术让生活变得更简单。">
+      <Head>
+        <title>彼得潘的永无岛</title>
+      </Head>
       <main>
-        <HomepageFeatures />
+        <WelcomePage />
       </main>
     </Layout>
   );
